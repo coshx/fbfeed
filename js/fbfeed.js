c11d673d182ed4b26ce447c78c0e17802de9fc7a
Ben Taitelbaum and Dan Epstein <dev+ben+dan@coshx.com>
2012-09-14 10:44:44 -0400

    debugging facebook init

--- js/fbfeed.js
+++ js/fbfeed.js
@@ -1,35 +1,7 @@
 var FbFeed = {};
 
 FbFeed.init = function() {
-  window.fbAsyncInit = function() {
-    Parse.FacebookUtils.init({
-      appId      : '400503560015485', // App ID
-      channelUrl : '//coshx.github.com/fbfeed/channel.html', // Channel File
-      status     : true, // check login status
-      cookie     : true, // enable cookies to allow the server to access the session
-      xfbml      : true  // parse XFBML
-    });
 
-    /* see https://parse.com/questions/unable-to-access-fb-graph-api-due-to-inactive-access-tokens */
-    FB.init({
-      appId      : '400503560015485', // App ID
-      channelUrl : '//coshx.github.com/fbfeed/channel.html', // Channel File
-      status     : true, // check login status
-      cookie     : true, // enable cookies to allow the server to access the session
-      xfbml      : true  // parse XFBML
-    });
-
-    // Additional initialization code here
-  };
-
-  // Load the SDK Asynchronously
-  (function(d){
-     var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
-     if (d.getElementById(id)) {return;}
-     js = d.createElement('script'); js.id = id; js.async = true;
-     js.src = "//connect.facebook.net/en_US/all.js";
-     ref.parentNode.insertBefore(js, ref);
-  }(document));
 }
 
 FbFeed.login = function() {
