1cdcf9f152384b398095a8309474295642fe7512
Ben Taitelbaum and Dan Epstein <dev+ben+dan@coshx.com>
2012-09-14 10:51:58 -0400

    switches to synchronous init

--- js/fbfeed.js
+++ js/fbfeed.js
@@ -8,15 +8,6 @@ FbFeed.init = function() {
     cookie     : true, // enable cookies to allow the server to access the session
     xfbml      : true  // parse XFBML
   });
-
-  // Load the SDK Asynchronously
-  (function(d){
-     var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
-     if (d.getElementById(id)) {return;}
-     js = d.createElement('script'); js.id = id; js.async = true;
-     js.src = "//connect.facebook.net/en_US/all.js";
-     ref.parentNode.insertBefore(js, ref);
-  }(document));
 }
 
 FbFeed.login = function(e) {
